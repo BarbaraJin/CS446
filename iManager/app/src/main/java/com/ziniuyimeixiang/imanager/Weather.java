47b82f12c6f9b1974834967e1712459975dc0c78
BarbaraJin <j_meixiang@outlook.com>
2018-02-24 17:45:16 -0500

    edit weather xml

--- iManager/app/src/main/java/com/ziniuyimeixiang/imanager/Weather.java
+++ iManager/app/src/main/java/com/ziniuyimeixiang/imanager/Weather.java
@@ -1,20 +1,28 @@
 package com.ziniuyimeixiang.imanager;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import java.util.Observable;
 import java.util.Observer;
 
 public class Weather extends AppCompatActivity implements Observer {
-    Model mModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        mModel = Model.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
+
     public void update(Observable o, Object arg) {
 
     }
+
 }
