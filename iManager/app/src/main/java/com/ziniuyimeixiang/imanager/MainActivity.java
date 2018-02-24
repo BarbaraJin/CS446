ec1db23d05012db3af93483fd8a64216daf649fc
BarbaraJin <j_meixiang@outlook.com>
2018-02-24 12:52:50 -0500

    add basic botton in main page
    need to change!

--- iManager/app/src/main/java/com/ziniuyimeixiang/imanager/MainActivity.java
+++ iManager/app/src/main/java/com/ziniuyimeixiang/imanager/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ziniuyimeixiang.imanager;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    // TODO need to change main activity's view
+    public void weatherBottonClicked(View view){
+        Intent weatherIntent = new Intent(MainActivity.this, Weather.class);
+        startActivity(weatherIntent);
+    }
+
+    public void calenderBottonClicked(View view){
+        Intent weatherIntent = new Intent(MainActivity.this, Calender.class);
+        startActivity(weatherIntent);
+    }
+
+    public void alarmBottonClicked(View view){
+        Intent weatherIntent = new Intent(MainActivity.this, Alarm.class);
+        startActivity(weatherIntent);
     }
 }
